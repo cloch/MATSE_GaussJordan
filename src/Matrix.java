83bd85c9ede87cfaf7eb43c0f22d480a57018405
Nephistar <eva.mueller147@gmx.de>
2019-06-18 11:56:19 +0200

    created stubs

--- src/Matrix.java
+++ src/Matrix.java
@@ -1,2 +1,24 @@
 public class Matrix {
+    private double[] values;
+    private int dimX;
+    private int dimY;
+
+    public void gauss(){
+
+    }
+    public void jordan(){
+
+    }
+    private void norm(){
+
+    }
+    private void combine(){
+
+    }
+    private void sortRows(){
+
+    }
+    private void print(){
+
+    }
 }
