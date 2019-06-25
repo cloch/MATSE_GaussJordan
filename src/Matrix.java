266ca84ba1f747c6c876e7ece6235d66f0be54d4
Eva Zimmermann (uib18557) <uib18557@contiwan.com>
2019-06-25 10:20:18 +0200

    implemented norm function

--- src/Matrix.java
+++ src/Matrix.java
@@ -10,7 +10,22 @@ public class Matrix {
 
     }
     private void norm(){
-
+        /*
+        for every row x find first value different from 0
+        divide all values in the same row by this value
+         */
+        for (int y = 0; y < dimY; y++){
+            int firstX = 0;
+            while (Math.abs(values[y][firstX]) < 2 * Double.MIN_VALUE){ //value close to 0
+                values[y][firstX] = 0;
+                firstX++;
+            }
+            double divisor = values[y][firstX];
+            values[y][firstX] = 1;
+            for (int x = firstX + 1; x < dimX; x++){
+                values[y][x] = values[y][x] / divisor;
+            }
+        }
     }
     private void combine(){
 
