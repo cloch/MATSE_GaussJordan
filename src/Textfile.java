6f034becca3482f5db562ebc85f84b58bf924404
Christian Loch (uib05376) <christian.loch@continental-corporation.com>
2019-06-27 11:14:58 +0200

    Fixed indices in Textfile.

--- src/Textfile.java
+++ src/Textfile.java
@@ -6,22 +6,11 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * The type Textfile.
- */
 public class Textfile {
 	private String filepath;
-	/**
-	 * The Matrices.
-	 */
 	public Matrix[] matrices;
 	private File input;
 
-	/**
-	 * Instantiates a new Textfile.
-	 *
-	 * @param path the path
-	 */
 	public Textfile(String path) {
 		try{
 			this.filepath = path;
@@ -34,18 +23,6 @@ public class Textfile {
 			IOe.printStackTrace();
 		}
 
-	}
-
-	public Matrix GenerateMatrix()  throws Exception{
-		ArrayList<String> FContent = ReadFile();
-		ArrayList<ArrayList<Double>> lines = BuildMatLines(FContent);
-		Matrix m = new Matrix(lines.size(), lines.get(0).size());
-		for(int i = 0; i < lines.size(); i++){
-			for (int j = 0; j < lines.get(i).size(); j++){
-				m.setValue(i, j, lines.get(i).get(j));
-			}
-		}
-		return m;
 	}
 
 	private ArrayList<String> ReadFile() throws IOException{
@@ -62,7 +39,7 @@ public class Textfile {
 	}
 
 	private ArrayList<Double> GetVariables(String s){
-		ArrayList<Double> res = new ArrayList<>();
+		ArrayList<Double> res = new ArrayList<>(); 
 		Matcher m = Pattern.compile("[0-9]+(\\.[0-9]+)*").matcher(s);
 		while (m.find()){
 			res.add(Double.valueOf(m.group()));
@@ -76,6 +53,18 @@ public class Textfile {
 			res.add(GetVariables(s));
 		}
 		return res;
+	}
+
+	public Matrix GenerateMatrix(ArrayList<Double> list)  throws Exception{
+		ArrayList<String> FContent = ReadFile();
+		ArrayList<ArrayList<Double>> lines = BuildMatLines(FContent);
+		Matrix m = new Matrix(lines.get(0).size(), lines.size());
+		for (int i = 0; i < m.getDimY(); i++){
+			for (int j = 0; j < m.getDimX(); j++){
+				m.setValue(j, i, lines.get(i).get(j));
+			}
+		}
+		return m;
 	}
 
 	private boolean CheckForEqualRows(ArrayList<ArrayList<Double>> list){
@@ -92,6 +81,6 @@ public class Textfile {
 	}
 
 	private boolean IsValid(String s){
-		return Pattern.matches("([0-9]+(\\.[0-9]+)*( |\t)*){2,}", s);
+		return Pattern.matches("([0-9]+(\\.[0-9]+)*( |\t)*){2,}", s);    	
 	}
 }
